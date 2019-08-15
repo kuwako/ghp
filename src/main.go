e2a53446ca9ac4d7e936989ccf7c4a3c78a09a4d
kuwako <m.kuwako0702@gmail.com>
2019-08-15 11:28:09 +0900

    first commit

---
+++ src/main.go
@@ -0,0 +1,14 @@
+package ghp
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("this is ghp")
+}
+
+func StrRepeat(input string, multiplier int) string {
+	return strings.Repeat(input, multiplier)
+}
